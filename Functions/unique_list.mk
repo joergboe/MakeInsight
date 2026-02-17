6338b90f9e3d72a8a12228ffd0f5a698644b2b12
Joerg Boehmer <joerg.boehmer@snafu.de>
2026-02-17 21:39:03 +0100

    Corrections

--- Functions/unique_list.mk
+++ Functions/unique_list.mk
@@ -21,7 +21,7 @@ endef
 list1 =
 list1a ::= $(space)
 list2 = m1
-list3 = m1 m2 m3 m3
+list3 = m1 m2 m3 m3   #
 list4 = . src . src src/m1
 list5 = m1 m2 m3 . src . src , src/m$$1 m$$1 , \# src/m$$1
 list6 =    111 222     333 , $(space) 444 $(multiline) 555 \#66 \\\#666 7:7 8;8 9*9 a$$a b%b c\c d\\d e(@)e\
@@ -66,32 +66,32 @@ prepare_list_for_sort = $(subst $(space),$(nl),$(strip $1))
 
 $(info list1 = '$(list1)')
 # NOTE: use simple variables only, recursively expanded variables are exported late!
-export list1 ::= $(call prepare_list_for_sort,$(list1))
-$(info uniq  = '$(call uniq,list1)')
+export list1_ ::= $(call prepare_list_for_sort,$(list1))
+$(info uniq  = '$(call uniq,list1_)')
 
 $(info list1a = '$(list1a)')
-export list1a ::= $(call prepare_list_for_sort,$(list1a))
-$(info uniq   = '$(call uniq,list1a)')
+export list1a_ ::= $(call prepare_list_for_sort,$(list1a))
+$(info uniq   = '$(call uniq,list1a_)')
 
 $(info list2 = '$(list2)')
-export list2 ::= $(call prepare_list_for_sort,$(list2))
+export list2_ ::= $(call prepare_list_for_sort,$(list2))
 $(info $(value list2))
-$(info uniq  = '$(call uniq,list2)')
+$(info uniq  = '$(call uniq,list2_)')
 
 $(info list3 = '$(list3)')
-export list3 ::= $(call prepare_list_for_sort,$(list3))
-$(info uniq  = '$(call uniq,list3)')
+export list3_ ::= $(call prepare_list_for_sort,$(list3))
+$(info uniq  = '$(call uniq,list3_)')
 
 $(info list4 = '$(list4)')
-export list4 ::= $(call prepare_list_for_sort,$(list4))
-$(info uniq  = '$(call uniq,list4)')
+export list4_ ::= $(call prepare_list_for_sort,$(list4))
+$(info uniq  = '$(call uniq,list4_)')
 
 $(info list5 = '$(list5)')
-export list5 ::= $(call prepare_list_for_sort,$(list5))
-$(info uniq  = '$(call uniq,list5)')
+export list5_ ::= $(call prepare_list_for_sort,$(list5))
+$(info uniq  = '$(call uniq,list5_)')
 
 $(info list6 = '$(list6)')
-export list6 ::= $(call prepare_list_for_sort,$(list6))
-$(info uniq  = '$(call uniq,list6)')
+export list6_ ::= $(call prepare_list_for_sort,$(list6))
+$(info uniq  = '$(call uniq,list6_)')
 
 target:
