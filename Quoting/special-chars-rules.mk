e4f83b336b95f9dd8934412d1309744a5fc5f5f4
Joerg Boehmer <joerg.boehmer@snafu.de>
2026-03-25 11:17:16 +0100

    special chars in rules updated

--- Quoting/special-chars-rules.mk
+++ Quoting/special-chars-rules.mk
@@ -1,15 +1,15 @@
-# Use of characters special to make in targets and prerequisites
+# Use of special characters in targets and prerequisites
 
 # Usage:
-# Demonstration of rules to generates files with non special characters
+# Demonstration of rules to generates files with special characters
 # make -f special-chars-rules.mk
 
-# Check that all files are recognized as up to date: Nothing to be done for 'all'.
+# Check that all files are recognized as up to date.
+# Expected: Nothing to be done for 'all'.
 # make -f special-chars-rules.mk
 
 # Show the generated files
 # make -f special-chars-rules.mk show
-
 # Cleanup
 # make -f special-chars-rules.mk clean
 
@@ -17,59 +17,54 @@
 ifneq (,$(findstring show,$(MAKECMDGOALS)))
   $(info Characters: :, # and <space>)
   $(info Files with special char (to be escaped in target and in prerequisites):)
-  specialfiles := $(wildcard filesp1*s)
-  $(info $(specialfiles))
+  $(info $(wildcard filesp1*s))
   $(info )
   $(info Files with backslash preceding special char (to be escaped in target and in prerequisites):)
   $(info 2N+1 preceding backslashes represent N backslashes and the special char is taken literally)
-  specialfiles := $(wildcard filesp1*sb*)
-  $(info $(specialfiles))
+  $(info $(wildcard filesp1*sb*))
   $(info )
   $(info Characters: <tab>)
   $(info Files with decayed tab character:)
-  specialfiles := $(wildcard filesp2*)
-  $(info $(specialfiles))
+  $(info $(wildcard filesp2*))
   $(info )
   $(info Character to be escaped in prerequisites only: | )
   $(info Special characters to make that must be escaped in prerequisites:)
-  $(info 2N+1 preceding backslashes represent N backslashes and the special char is taken literally)
-  $(info !N preceding backslashes represent N backslashes in targets!)
-  specialfiles := $(wildcard filesp3*)
+  $(info Prerequisites: 2N+1 preceding backslashes represent N backslashes and the special char is taken literally.)
+  $(info Targets: N preceding backslashes represent N backslashes!)
+  $(info $(wildcard filesp3*))
   $(info $(specialfiles))
   $(info )
   $(info Character to be escape in targets only: % )
   $(info Special characters to make that must be escaped in target only:)
-  $(info 2N+1 preceding backslashes represent N backslashes and the special char is taken literally)
-  $(info !N preceding backslashes represent N backslashes in prerequisites!)
-  specialfiles := $(wildcard filesp4*)
-  $(info $(specialfiles))
+  $(info Targets: 2N+1 preceding backslashes represent N backslashes and the special char is taken literally.)
+  $(info Prerequisites: N preceding backslashes represent N backslashes!)
+  $(info $(wildcard filesp4*.s))
+  $(info $(wildcard filesp4*.o))
   $(info )
-  $(info No escaping possible for characters: = and ; )
-  $(info filesp1\=s filesp1\;s)
-  $(info filesp1\=s: #*** recipe commences before first target.  Stop.)
-  $(info filesp1\;s: #*** missing separator.  Stop.)
+  $(info No direct escaping possible for characters: = and ; )
+  $(info filesp5\=s: #*** recipe commences before first target.  Stop.)
+  $(info filesp5\;s: #*** missing separator.  Stop.)
+  $(info $(wildcard filesp5*))
   $(info )
 endif
 
 #.POSIX:
 .SUFFIXES:
 .PHONY: all show
-all: makespecials makespecials2 makespecials3 makespecials4 makespecials5
+all: filemakesp1 filemakesp2 filemakesp3 filemakesp4 filemakesp5
 
 show:
 
 # special chars to make - escape with backslash in target and prerequisite
-.PHONY: makespecials
-makespecials: filesp1\:s filesp1\#s filesp1\ s\
-              filesp1\\\:sb filesp1\\\\\#sbb filesp1\\\\\\\ sbbb
-	@echo '$@'
+filemakesp1: filesp1\:s filesp1\#s filesp1\ s filesp1\\\:sb filesp1\\\\\#sbb filesp1\\\\\\\ sbbb
+	touch '$@'
 	@echo '$$^: $^'
 	@echo '$$<: $<'
 	@echo
 
 filesp1\:s:
-	# Special charaters to make must be quoted to make with a single backspace
-	# Colon is a reserved word in the shell, thus it is allowed to use unquoted as second word.
+	# The special characters <space>, # and : must be quoted in targets and prerequisites with a single backslash.
+	# NOTE: Colon is a reserved word in the shell, thus it is allowed to use unquoted as second word.
 	touch $@ # : no shell quoting required
 filesp1\#s:
 	touch '$@' # # - shell quoting required
@@ -77,8 +72,8 @@ filesp1\ s:
 	touch '$@' # space  - shell quoting required
 	@echo
 filesp1\\\:sb:
-	# Backslashes that would otherwise quote special characters can be quoted with more backslashes.
-	# 2N+1 preceding backslashes represents N backslashes and the literal
+	# NOTE: Backslashes that would otherwise quote special characters can be quoted with more backslashes.
+	# 2N+1 preceding backslashes represents N backslashes and the literal special character.
 	touch '$@' # One backslash
 filesp1\\\\\#sbb:
 	touch '$@' # Two backslashes
@@ -86,77 +81,73 @@ filesp1\\\\\\\ sbbb:
 	touch '$@' # Three backslashes
 
 # tab character
-.PHONY: makespecials2
-makespecials2: filesp2\ s1 filesp2\ s2 filesp2\ s3
-	@echo '$@'
+filemakesp2: filesp2\ s1 filesp2\ s2 filesp2\ s3
+	touch '$@'
 	@echo '$$^: $^'
 	@echo '$$<: $<'
 	@echo
 
-#filesp2\	s1 'filesp2       s1', needed by 'makespecials2'.  Stop.
+#filesp2\	s1 'filesp2       s1', needed by 'filemakesp2'.  Stop.
 filesp2\	s1:
-	# tab character decays to a single space in targets
+	# NOTE: tab character decays to a single space in targets - a tab character can not be used in targets!
 	touch '$@'
 filesp2\	s2 filesp2\	s3:
 	touch '$@'
 
 # special to make - escape in prerequisite only
-.PHONY: makespecials3
-makespecials3: filesp3\|o filesp3\\\|ob
-	@echo '$@'
+filemakesp3: filesp3\|s filesp3\\\|sb
+	touch '$@'
 	@echo '$$^: $^'
 	@echo '$$<: $<'
 	@echo
-# filesp3\\|o # a pure | character is not allowed in prerequisites -> Order-only target
+# filesp3\\|s # a pure | character is not allowed in prerequisites -> Order-only target
 
-filesp3|o:
-	# special to make - escape in prerequisite only
+filesp3|s:
+	# The | character must be escaped in prerequisite only.
 	touch '$@' # in target no backslash required!
-filesp3\|ob:
-	touch '$@' # in target no backslash required - one backslash
+filesp3\|sb:
+	touch '$@' # NOTE: One backslash before | - yields one backslash before | in targets.
 
 # special to make - escape in target only
-.PHONY: makespecials4
-makespecials4: filesp4%sp1.o filesp4\%spb2.o filesp4\\%spbb3.o
-	@echo '$@'
+filemakesp4: filesp4%s.o filesp4\%sb.o filesp4\\%sbb.o
+	# % character is special to make in targets only
+	# In targets 2N+1 preceding backslashes represents N backslashes and the special char is taken literally.
+	# NOTE: In prerequisites one backslash before % - yields one backslash before %.
+	touch '$@'
 	@echo '$$^: $^'
 	@echo '$$<: $<'
-	# % character is !not! special to make in prerequisites
+	# NOTE: One backslash before % - yields one backslash before % in prerequisites.
 	@echo
 
-filesp4\%sp1.o: filesp4%sp1
-#filesp4%sp1.o: filesp4%sp1    # this does not! work this makes implicit rules
+filesp4\%s.o: filesp4%s.s
+#filesp4%s: filesp4%s.s    # this does not! work this makes implicit rules
 	@echo '$$@ = $@:	$$< = $<'
 	touch $@ # literal %
-filesp4\\\%spb2.o: filesp4\%spb2
-#filesp4\\%spb2.o: filesp4\%spb2
+filesp4\\\%sb.o: filesp4\%sb.s
+#filesp4\\%sb.o: filesp4\%sb.s
 	@echo '$$@ = $@:	$$< = $<'
 	touch '$@' # one backspace + literal %
-filesp4\\\\\%spbb3.o: filesp4\\%spbb3
-#filesp4\\\\%spbb3.o: filesp4\\%spbb3
+filesp4\\\\\%sbb.o: filesp4\\%sbb.s
+#filesp4\\\\%sbb.o: filesp4\\%sbb.s
 	@echo '$$@ = $@:	$$< = $<'
 	touch '$@' # two backspaces + literal %
 
-filesp4\%sp1:
-#filesp4%sp1:
-	# % character is special to make in targets only
-	# 2N+1 preceding backslashes represents N backslashes and the special char is taken literally
-	# 2N  preceding backslashes represents N backslashes and the literal % !!!"
+filesp4\%s.s:
+#filesp4%s.s:
 	touch $@ # literal %
-filesp4\\\%spb2:
-#filesp4\\%spb2:
+filesp4\\\%sb.s:
+#filesp4\\%sb.s:
 	touch '$@' # one backspace + literal %
-filesp4\\\\\%spbb3:
-#filesp4\\\\%spbb3:
+filesp4\\\\\%sbb.s:
+#filesp4\\\\%sbb.s:
 	touch '$@' # two backspaces + literal %
 
 # some expansion hacks
-.PHONY: makespecials5
-makespecials5: filesp5$(firstword =)s filesp5$(firstword %)s\
+filemakesp5: filesp5$(firstword =)s filesp5$(firstword %)s\
 filesp5\$(firstword =)sb filesp5\$(firstword %)sb
 #filesp5$(firstword |)s # this is recognized as order only rule anyway
 #fiesp5$(firstword ;)s
-	@echo '$@'
+	touch '$@'
 	@echo '$$^: $^'
 	@echo '$$<: $<'
 	@echo
@@ -177,4 +168,4 @@ filesp5\\\$(firstword %)sb:
 #	touch '$@'
 
 .PHONY: clean
-clean:; rm -fv filesp1* filesp2* filesp3* filesp4* filesp5*
+clean:; rm -fv filesp1* filesp2* filesp3* filesp4* filesp5* filemakesp*
